205740b0982bdc20d24c7e1e4bfcc27eb4af40b5
Geordie Rogers <geordie@grogers.xyz>
2018-05-01 23:58:53 +1200

    Added quick links

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# newTab
-A personalised new tab page
+# Quick Earth
+
+Quick Earth is a personalised new tab page that shows an assortion of quick links infront of google pretty earth images.
+
+## Install
+1. Download the zip file and extract it or clone the repository
+2. Navigate to `chrome://extensions` and enable developer tools in the top righthand corner
+3. Click load unpacked and select the folder containg the extension
+4. Done
--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-// Get a random ocation from the locations file
+// Get a random location from the locations file
 var randomID = Math.floor(Math.random() * (locations.length - 1000)) + 1000;
 var loc = locations[randomID];
 
 //Update webpage with new location
-document.getElementById("bg").style.backgroundImage = "url("+loc.image+")";
+document.getElementById("bg").style.backgroundImage = "url("+loc.image+")"; //Background Image
+//If a region is provided display it
 if(loc.region===""){
 	document.getElementById("region").innerHTML = "";
 } else {
 	document.getElementById("region").innerHTML = loc.region+", ";
 }
-document.getElementById("country").innerHTML = loc.country;
-document.getElementById("location").setAttribute('href', loc.map);
-document.getElementById("copy").innerHTML = "&copy; "+loc.copyright;
+document.getElementById("country").innerHTML = loc.country; //Country
+document.getElementById("location").setAttribute('href', loc.map); //Google Maps link
+document.getElementById("copy").innerHTML = "&copy; "+loc.copyright; //Copyright Information
