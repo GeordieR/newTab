7b4ee87f789a257cb3c372c9ded50839f9c39a50
Geordie Rogers <geordie@grogers.xyz>
2018-05-02 13:46:09 +1200

    Added offline compatibility

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 var randomID = Math.floor(Math.random() * (locations.length - 1000)) + 1000;
 var loc = locations[randomID];
 
-//Update webpage with new location
-document.getElementById("bg").style.backgroundImage = "url("+loc.image+")"; //Background Image
-//If a region is provided display it
-if(loc.region===""){
-	document.getElementById("region").innerHTML = "";
-} else {
-	document.getElementById("region").innerHTML = loc.region+", ";
+
+if(navigator.onLine){
+	//Update webpage with new location
+	document.getElementById("bg").style.backgroundImage = "url("+loc.image+")"; //Background Image
+	//If a region is provided display it
+	if(loc.region===""){
+		document.getElementById("region").innerHTML = "";
+	} else {
+		document.getElementById("region").innerHTML = loc.region+", ";
+	}
+	document.getElementById("country").innerHTML = loc.country; //Country
+	document.getElementById("location").setAttribute('href', loc.map); //Google Maps link
+	document.getElementById("copy").innerHTML = "&copy; "+loc.copyright; //Copyright Information
 }
-document.getElementById("country").innerHTML = loc.country; //Country
-document.getElementById("location").setAttribute('href', loc.map); //Google Maps link
-document.getElementById("copy").innerHTML = "&copy; "+loc.copyright; //Copyright Information
